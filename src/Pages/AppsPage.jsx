0d31f4de73470bf8ba645adaaac67d1e77c7aacb
Letto <karvatwellinton@hotmail.com>
2023-09-21 02:24:25 -0300

    Menu

--- src/Pages/AppsPage.jsx
+++ src/Pages/AppsPage.jsx
@@ -19,7 +19,10 @@ const AppsPage = ({ auth }) => {
                     <div className="app-box-bottom">
                         <br />
                         <p>SSIPTV</p>
-                        <button className='buttonApp' onClick={() => window.location.href = appLink}>Ir para o aplicativo</button>
+                        <button className="buttona">
+                            <span className="texta">Instalação</span>
+                            <svg className="arrowa" viewBox="0 0 448 512" height="1em" xmlns="http://www.w3.org/2000/svg"><path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"></path></svg>
+                        </button>
                     </div>
                 </div>
 
@@ -30,7 +33,10 @@ const AppsPage = ({ auth }) => {
                     <div className="app-box-bottom">
                         <br />
                         <p>Smart Player</p>
-                        <button className='buttonApp' onClick={() => window.location.href = appLink}>Ir para o aplicativo</button>
+                        <button className="buttona">
+                            <span className="texta">Instalação</span>
+                            <svg className="arrowa" viewBox="0 0 448 512" height="1em" xmlns="http://www.w3.org/2000/svg"><path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"></path></svg>
+                        </button>
                     </div>
                 </div>
 
@@ -42,7 +48,10 @@ const AppsPage = ({ auth }) => {
                         <br />
                         <p>xciptv player</p>
 
-                        <button className='buttonApp' onClick={() => window.location.href = appLink}>Ir para o aplicativo</button>
+                        <button className="buttona">
+                            <span className="texta">Instalação</span>
+                            <svg className="arrowa" viewBox="0 0 448 512" height="1em" xmlns="http://www.w3.org/2000/svg"><path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"></path></svg>
+                        </button>
                     </div>
                 </div>
 
@@ -53,7 +62,10 @@ const AppsPage = ({ auth }) => {
                     <div className="app-box-bottom">
                         <br />
                         <p>Clouddy</p>
-                        <button className='buttonApp' onClick={() => window.location.href = appLink}>Ir para o aplicativo</button>
+                        <button className="buttona">
+                            <span className="texta">Instalação</span>
+                            <svg className="arrowa" viewBox="0 0 448 512" height="1em" xmlns="http://www.w3.org/2000/svg"><path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"></path></svg>
+                        </button>
                     </div>
                 </div>
 
@@ -65,7 +77,10 @@ const AppsPage = ({ auth }) => {
                     <div className="app-box-bottom">
                         <br />
                         <p>Web Cast Video</p>
-                        <button className='buttonApp' onClick={() => window.location.href = appLink}>Ir para o aplicativo</button>
+                        <button className="buttona">
+                            <span className="texta">Instalação</span>
+                            <svg className="arrowa" viewBox="0 0 448 512" height="1em" xmlns="http://www.w3.org/2000/svg"><path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"></path></svg>
+                        </button>
                     </div>
                 </div>
 
@@ -77,7 +92,10 @@ const AppsPage = ({ auth }) => {
                     <div className="app-box-bottom">
                         <br />
                         <p>Smart Tv Clube</p>
-                        <button className='buttonApp' onClick={() => window.location.href = appLink}>Ir para o aplicativo</button>
+                        <button className="buttona">
+                            <span className="texta">Instalação</span>
+                            <svg className="arrowa" viewBox="0 0 448 512" height="1em" xmlns="http://www.w3.org/2000/svg"><path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"></path></svg>
+                        </button>
                     </div>
                 </div>
             </div>
